421647ca6e24509ef48a2e48d3064cc9fa606969
alan nunes <alansilva2896@gmail.com>
2024-09-11 01:46:45 -0300

    2_FUNDAMENTOS

---
+++ 2_FUNDAMENTOS/index.js
@@ -0,0 +1,29 @@
+"use strict";
+// 1 numbers
+let x = 10;
+console.log(x);
+x = 16;
+console.log(typeof x);
+const y = 15.584848;
+console.log(typeof y);
+console.log(y);
+console.log(y.toPrecision(3));
+// 2 string
+const firstName = "Alan";
+console.log(firstName.toUpperCase());
+let fullName;
+const lastName = "Nunes";
+fullName = firstName + " " + lastName;
+console.log(fullName.toUpperCase());
+// 3 boolean
+let a = false;
+console.log(a);
+console.log(typeof a);
+a = true;
+console.log(a);
+// 4 inference e anotation
+let ann = "teste";
+let inf = "teste";
+// ann = 1
+// inf = 1
+console.log("testando");
